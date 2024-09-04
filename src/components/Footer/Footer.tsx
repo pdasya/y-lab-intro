692e2a8eb533709be236ff4ecb69d9f427682c08
Daria Pashkova <dar.pashcovaa@gmail.com>
2024-09-04 15:22:19 +0900

    feat: add footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,27 @@
+import { FC } from "react";
+import styles from "./Footer.module.scss";
+
+const Footer: FC = () => {
+  return (
+    <div className={styles.footerWrapper}>
+      <a href="https://github.com/pdasya" className={styles.footerGithub}>
+        <img
+          src="/github.png"
+          alt="github image"
+          className={styles.footerGithubImage}
+        ></img>
+        pdasya
+      </a>
+      <p className={styles.footerYear}>2024</p>
+      <a href="https://university.ylab.io/" className={styles.footerYlab}>
+        <img
+          src="/ylab-university.png"
+          alt="ylab image"
+          className={styles.footerYlabImage}
+        ></img>
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
--- src/views/Main.tsx
+++ src/views/Main.tsx
@@ -1,12 +1,17 @@
 import { FC } from "react";
 import Form from "../components/Form/Form";
 import styles from "./Main.module.scss";
+import Footer from "../components/Footer/Footer";
+import "../App.css";
 
 const Main: FC = () => {
   return (
-    <main className={styles.mainWrapper}>
-      <Form />
-    </main>
+    <>
+      <main className={styles.mainWrapper}>
+        <Form />
+      </main>
+      <Footer />
+    </>
   );
 };
 
